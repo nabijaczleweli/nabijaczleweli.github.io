64997701d547565659ccc1d8d7b224b4db1500a2
nabijaczleweli <nabijaczleweli@gmail.com>
2015-11-17 00:30:39 +0100

    Don't do that, that's stupid

--- assets/script/document.js
+++ assets/script/document.js
@@ -1,7 +1,3 @@
-var global_root;
-if(global_root === undefined)
-	global_root = location.href.match(/([a-z]{4,5}:\/\/).*[a-z]+\.[a-z]+\.[a-z]+/ig)[0];
-
 var __footer_text;
 function insert_footer(cls) {
 	function __insert_footer(cls) {
@@ -15,7 +11,7 @@ function insert_footer(cls) {
 		__insert_footer(cls)
 	else {
 		const request = new XMLHttpRequest();
-		request.open('GET', global_root + '/assets/document/footer.htm');
+		request.open('GET', '/assets/document/footer.htm');
 		request.onload = function(load) {
 			__footer_text = load.target.responseText;
 			__insert_footer(cls);
