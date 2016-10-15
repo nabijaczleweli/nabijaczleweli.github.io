4ddb62cc9eca36587ea482daf1c60976b5419e29
nabijaczleweli <nabijaczleweli@gmail.com>
2016-10-16 00:36:01 +0200

    Add dishub crate

--- src/index.html.pp
+++ src/index.html.pp
@@ -52,6 +52,8 @@ Documentation for my various projects:
 	    Display images in your terminal, kind of.)
 	DOC(blake-rs, blake, blake,
 	    Implementation of the <a href="https://131002.net/blake">BLAKE</a> hash function for Rust via FFI)
+	DOC(dishub, dishub, dishub,
+	    App for posting GitHub activity on Discord)
 	DOC(tweetr, tweetr, tweetr,
 	    tweetr is a platform that allows you to create and queue tweets to be shared when YOU want.
 	    You create content when you have time and then use FOSS and NOT pay whatever-ridiculous amount of $$$ for posting them automatically.)
