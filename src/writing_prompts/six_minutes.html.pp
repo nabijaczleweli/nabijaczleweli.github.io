8e104a115e2854e066f0387c3f1353148c4b9dfd
nabijaczleweli <nabijaczleweli@gmail.com>
2016-12-28 01:55:49 +0100

    Fix typo in Six Minutes writing prompt

--- src/writing_prompts/six_minutes.html.pp
+++ src/writing_prompts/six_minutes.html.pp
@@ -13,7 +13,7 @@ work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
 #include "../writing/writing.h"
 
 
-#define TODO add RSS-PUB-DATE
+#define RSS_PUB_DATE Wed, 28 Dec 2016 01:54:52 +0100
 
 
 BOILERPLATE(Six Minutes, Six Minutes - writing prompt)
