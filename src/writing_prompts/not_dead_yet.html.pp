0c94c9aabdf54346573b2ddfb256156c2afe0600
nabijaczleweli <nabijaczleweli@gmail.com>
2016-12-29 22:03:12 +0100

    Fix formatting for Get up, soldier WP

--- src/writing_prompts/not_dead_yet.html.pp
+++ src/writing_prompts/not_dead_yet.html.pp
@@ -13,7 +13,7 @@ work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
 #include "../writing/writing.h"
 
 
-#define RSS_PUB_DATE Thu, 29 Dec 2016 21:51:49 +0100
+#define RSS_PUB_DATE Thu, 29 Dec 2016 21:51:50 +0100
 
 
 BOILERPLATE(Get up， soldier, Get up， soldier - writing prompt)
