6cec52a95c0940c99d64900e916bb84df5a884a8
nabijaczleweli <nabijaczleweli@gmail.com>
2016-12-29 21:53:17 +0100

    Add Get up, soldier Writing Prompt

---
+++ src/writing_prompts/not_dead_yet.html.pp
@@ -0,0 +1,30 @@
+<!--
+nabijaczleweli.xyz (c) by nabijaczleweli@gmail.com (nabijaczleweli)
+​
+nabijaczleweli.xyz is licensed under a
+Creative Commons Attribution 4.0 International License.
+​
+You should have received a copy of the license along with this
+work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
+-->
+
+
+#include "../common.h"
+#include "../writing/writing.h"
+
+
+#define RSS_PUB_DATE Thu, 29 Dec 2016 21:51:49 +0100
+
+
+BOILERPLATE(Get up， soldier, Get up， soldier - writing prompt)
+
+
+<style type="text/css">
+#include "column.css"
+</style>
+
+#include "not_dead_yet.html"
+
+
+WRITING_CUSTOM_NAME_END(all stories)
+BOILERPLATE_END()
