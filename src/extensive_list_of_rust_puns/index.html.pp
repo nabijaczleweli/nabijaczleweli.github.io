7df30f7c5f16656c7da08ef824db1fe9834d37ed
nabijaczleweli <nabijaczleweli@gmail.com>
2017-01-23 19:57:08 +0100

    <em> is not <code> ffs

--- src/extensive_list_of_rust_puns/index.html.pp
+++ src/extensive_list_of_rust_puns/index.html.pp
@@ -20,7 +20,7 @@ Extensive list of <a href="//www.rust-lang.org">Rust</a> puns,
 as <a href="//chat.stackoverflow.com/transcript/62927?m=35242527#35242527">requested</a>
 by <a href="//chat.stackoverflow.com/users/2408867">Lukas Kalbertodt</a>:
 <ul>
-  <li>"Kept wondering what was preventing me from sharing a <em>Cell</em> with multiple threads, but then I let the idea <em>Sync</em> in."
+  <li>"Kept wondering what was preventing me from sharing a <code>Cell</code> with multiple threads, but then I let the idea <code>Sync</code> in."
        -- <a href="//chat.stackoverflow.com/users/1233251">E_net4</a>
         - <a href="//chat.stackoverflow.com/transcript/message/35242477#35242477">22.01.17 11:34 PM</a></li>
 </ul>
