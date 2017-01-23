535815c05304e86dca60903b7f9268fa947d8884
nabijaczleweli <nabijaczleweli@gmail.com>
2017-01-23 19:48:23 +0100

    Add Extensive List of Rust Puns

---
+++ src/extensive_list_of_rust_puns/index.html.pp
@@ -0,0 +1,29 @@
+<!--
+nabijaczleweli.xyz (c) by nabijaczleweli@gmail.com (nabijaczleweli)
+​
+nabijaczleweli.xyz is licensed under a
+Creative Commons Attribution 4.0 International License.
+​
+You should have received a copy of the license along with this
+work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
+-->
+
+
+#include "../common.h"
+#include "../writing/writing.h"
+
+
+BOILERPLATE(Extensive list of Rust puns, Results of many months spent infiltrating the Rastesian community)
+
+
+Extensive list of <a href="//www.rust-lang.org">Rust</a> puns,
+as <a href="//chat.stackoverflow.com/transcript/62927?m=35242527#35242527">requested</a>
+by <a href="//chat.stackoverflow.com/users/2408867">Lukas Kalbertodt</a>:
+<ul>
+  <li>"Kept wondering what was preventing me from sharing a <em>Cell</em> with multiple threads, but then I let the idea <em>Sync</em> in."
+       -- <a href="//chat.stackoverflow.com/users/1233251">E_net4</a>
+        - <a href="//chat.stackoverflow.com/transcript/message/35242477#35242477">22.01.17 11:34 PM</a></li>
+</ul>
+
+
+BOILERPLATE_END()
