118a66277aec99da0ef0f0a0b6a6b4db70a5234e
nabijaczleweli <nabijaczleweli@gmail.com>
2017-02-07 18:00:21 +0100

    Use <samp> instead of <code> for monospace forcing

--- src/extensive_list_of_rust_puns/index.html.pp
+++ src/extensive_list_of_rust_puns/index.html.pp
@@ -20,10 +20,10 @@ Extensive list of <a href="//www.rust-lang.org">Rust</a> puns,
 as <a href="//chat.stackoverflow.com/transcript/62927?m=35242527#35242527">requested</a>
 by <a href="//chat.stackoverflow.com/users/2408867">Lukas Kalbertodt</a>:
 <ul>
-  <li>"Kept wondering what was preventing me from sharing a <code>Cell</code> with multiple threads, but then I let the idea <code>Sync</code> in."
+  <li>"Kept wondering what was preventing me from sharing a <samp>Cell</samp> with multiple threads, but then I let the idea <samp>Sync</samp> in."
        -- <a href="//chat.stackoverflow.com/users/1233251">E_net4</a>
         - <a href="//chat.stackoverflow.com/transcript/message/35242477#35242477">22.01.17 11:34 PM</a></li>
-  <li>"Indiana Jones raiders of the lost <code>Arc&lt;T&gt;</code>"
+  <li>"Indiana Jones and the Raiders of the Lost <samp>Arc&lt;T&gt;</samp>"
        -- <a href="//chat.stackoverflow.com/users/2408867">Lukas Kalbertodt</a>
         - <a href="//chat.stackoverflow.com/transcript/message/35262129#35262129">23.01.17 11:30 PM</a></li>
 </ul>
