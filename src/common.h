6fd5a28c601965801b226bad82fb3d9dc62788ea
nabijaczleweli <nabijaczleweli@gmail.com>
2017-03-03 01:35:52 +0100

    Replace JS icon HTML fetcher with raw HTML

--- src/common.h
+++ src/common.h
@@ -35,6 +35,32 @@
 #endif
 
 
+#define CMT // Copypasted from /assets/icons.htm
+#undef CMT
+#define BOILERPLATE_ICONS                                                                                 \
+    <link rel="apple-touch-icon" sizes="57x57" href="/assets/favicons/apple-touch-icon-57x57.png">        \
+    <link rel="apple-touch-icon" sizes="60x60" href="/assets/favicons/apple-touch-icon-60x60.png">        \
+    <link rel="apple-touch-icon" sizes="72x72" href="/assets/favicons/apple-touch-icon-72x72.png">        \
+    <link rel="apple-touch-icon" sizes="76x76" href="/assets/favicons/apple-touch-icon-76x76.png">        \
+    <link rel="apple-touch-icon" sizes="114x114" href="/assets/favicons/apple-touch-icon-114x114.png">    \
+    <link rel="apple-touch-icon" sizes="120x120" href="/assets/favicons/apple-touch-icon-120x120.png">    \
+    <link rel="apple-touch-icon" sizes="144x144" href="/assets/favicons/apple-touch-icon-144x144.png">    \
+    <link rel="apple-touch-icon" sizes="152x152" href="/assets/favicons/apple-touch-icon-152x152.png">    \
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon-180x180.png">    \
+    <link rel="icon" type="image/png" href="/assets/favicons/favicon-32x32.png" sizes="32x32">            \
+    <link rel="icon" type="image/png" href="/assets/favicons/android-chrome-192x192.png" sizes="192x192"> \
+    <link rel="icon" type="image/png" href="/assets/favicons/favicon-96x96.png" sizes="96x96">            \
+    <link rel="icon" type="image/png" href="/assets/favicons/favicon-16x16.png" sizes="16x16">            \
+    <link rel="manifest" href="/assets/favicons/manifest.json">                                           \
+    <link rel="mask-icon" href="/assets/favicons/safari-pinned-tab.svg" color="#5bbad5">                  \
+    <link rel="shortcut icon" href="/assets/favicons/favicon.ico">                                        \
+    <meta name="apple-mobile-web-app-title" content="nabijaczleweli">                                     \
+    <meta name="application-name" content="nabijaczleweli">                                               \
+    <meta name="msapplication-TileColor" content="#da532c">                                               \
+    <meta name="msapplication-TileImage" content="/assets/favicons/mstile-144x144.png">                   \
+    <meta name="msapplication-config" content="/assets/favicons/browserconfig.xml">                       \
+    <meta name="theme-color" content="#ffffff">
+
 #define BOILERPLATE(pagename, description, ...)                                                            \
 <!DOCTYPE html>                                                                                            \
 <html lang="en">                                                                                           \
@@ -48,7 +74,7 @@
                                                                                                            \
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" /> \
     <link href="/capitalism/assets/common.css" rel="stylesheet" />                                         \
-    <script type="text/javascript" src="/js/add_icons.js" id="icon-placeholder"></script>                  \
+    BOILERPLATE_ICONS                                                                                      \
     __VA_ARGS__                                                                                            \
   </head>                                                                                                  \
   <body>
