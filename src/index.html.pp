2e740fbb1860e04c748855a8cf153940efc59ef6
nabijaczleweli <nabijaczleweli@gmail.com>
2017-03-08 01:08:12 +0100

    Add pls-rs crate

--- src/index.html.pp
+++ src/index.html.pp
@@ -78,6 +78,8 @@ Documentation for my various projects:
 	DOC(tweetr, tweetr, tweetr,
 	    tweetr is a platform that allows you to create and queue tweets to be shared when YOU want.
 	    You create content when you have time and then use FOSS and NOT pay whatever-ridiculous amount of $$$ for posting them automatically)
+	DOC(pls-rs, pls, pls,
+	    Rust crate for parsing and writing the PLS playlist format)
 	DOC(md6-rs, md6, md6,
 	    Implementation of the <a href="https://groups.csail.mit.edu/cis/md6">MD6</a> hash function for Rust via FFI)
 </ul>
