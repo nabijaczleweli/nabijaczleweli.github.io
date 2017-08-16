dfb62b83357811b4e30af6357312171e8b995fb7
nabijaczleweli <nabijaczleweli@gmail.com>
2017-08-17 00:15:07 +0200

    Add ASCIIMath version of mandala algorighm

--- src/maths/mandalas.html.pp
+++ src/maths/mandalas.html.pp
@@ -14,24 +14,30 @@ work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
 
 #define RSS_PUB_DATE Tue, 13 Jun 2017 22:42:51 +0200
 
+#define MATHJAX(...) [​[​__VA_ARGS__​]​]
+
 #define SCANS_CSS_LINK    <link href="/capitalism/assets/scan.css" rel="stylesheet" />
 #define SCANS_SHOW_SCRIPT <script type="text/javascript" src="/capitalism/assets/scans_show.js"></script>
 #define MATHJAX_SCRIPT    <script type="text/javascript" \
                                   src="//cdn.rawgit.com/mathjax/MathJax/master/MathJax.js?config=/capitalism/assets/MathJax-config.js"></script>
 BOILERPLATE(Mandalas, Tonči Juričev-Grginꞌs Mandalas, SCANS_CSS_LINK SCANS_SHOW_SCRIPT MATHJAX_SCRIPT)
 
+<style>
+#include "../writing/indent.css"
+</style>
+
 
 <div>
-<p>
-&emsp;The Tonči Juričev-Grgin mandala generation algorithm can be used to, <em>drumroll please</em>, generate mandalas like this one:
+<p class="indented">
+The Tonči Juričev-Grgin mandala generation algorithm can be used to, <em>drumroll please</em>, generate mandalas like this one:
 </p>
 <a href="/capitalism/assets/mandalas/900x900.png" target="_blank">
 	<img src="/capitalism/assets/mandalas/900x900.png" class="bigimage" /></a><br />
 <small>Click for big version.</small>
-<p>
-&emsp;It uses a very simple process (see <a href="#show-scans">below</a>), which can be used in any
-	&lt;AsciiMath&gt;beta in NN ∖ {0}&lt;/AsciiMath&gt;-dimensional space, so, as an example, the same-sized mandala,
-	but in one more dimension (also with classical music atop it because it'd be unbearably bloody boring otherwise):<!--'-->
+<p class="indented">
+It uses a very simple process (see <a href="#show-scans">below</a>), which can be used in any
+MATHJAX(beta in NN ∖ {0})-dimensional space, so, as an example, the same-sized mandala,
+but in one more dimension (also with classical music atop it because it'd be unbearably bloody boring otherwise):<!--'-->
 </p>
 &emsp;<iframe width="560" height="315" src="//youtube.com/embed/D2Xk6O9x-yk?list=PL3chI5PWwNA_c1nG57ittUq4op7ZmIveC" frameborder="0" allowfullscreen
               style="max-width: 100%;">
@@ -43,10 +49,10 @@ BOILERPLATE(Mandalas, Tonči Juričev-Grginꞌs Mandalas, SCANS_CSS_LINK SCANS_S
 <strong>"How long does rendering a thing such as this take?"</strong>, one might ask.
 	Two answers, the short one - too sodding long, the not-quite-as-short one is that due to the process' inherent randomness<!--'-->
   one need run the algorithm body many times, an acceptable amount I found was:<br />
-	&lt;AsciiMath&gt;log(n_max) = |~log(prod_i size_i)~| + 1&lt;/AsciiMath&gt;,<br />
+	MATHJAX(log(n_max) = |~log(prod_i size_i)~| + 1),<br />
 	(which still doesn't perfectly fill more complex corner setups), so for<!--'-->
-	&lt;AsciiMath&gt;size = [900, 900, 900] => n_max = 10^10&lt;/AsciiMath&gt;,
-	which takes about &lt;AsciiMath&gt;7 1/2 h&lt;/AsciiMath&gt; on my PC if I try to not cook it (and I generally do).
+	MATHJAX(size = [900, 900, 900] => n_max = 10^10),
+	which takes about MATHJAX(7 1/2 h) on my PC if I try to not cook it (and I generally do).
 </p>
 
 <p>
@@ -54,7 +60,7 @@ A different question to be asked is
 	<strong>"How does one</strong><!--"-->
 	<span style="text-decoration: line-through;">laugh at the shitty code</span>
 	<strong>play around with the code that generates these myself?"</strong>.<!--"-->
-	Well, thanks to the wonders of the &lt;AsciiMath&gt;fr "Internet"&lt;/AsciiMath&gt;, you can! In most-to-least bad order:<br />
+	Well, thanks to the wonders of the MATHJAX(fr "Internet"), you can! In most-to-least bad order:<br />
 &emsp;<a href="//github.com/nabijaczleweli/mandalas" target="_blank">Here</a>'s an early version in C++,<!--'-->
 	featuring layers upon layers of terrible code from late-2015/early-2016. You probably don't want to try to build it, headaches afore.<!--'--><br />
 &emsp;<a href="/capitalism/assets/mandalas/mandalas-js.zip" target="_blank">Here</a>'s the very first version, in JavaScript,<!--'-->
@@ -71,7 +77,8 @@ A different question to be asked is
 	<em>really</em> big in a couple places.<br />
 &emsp;<a href="//youtu.be/D2Xk6O9x-yk&list=PL3chI5PWwNA_c1nG57ittUq4op7ZmIveC" target="_blank">Here</a>'s<!--'-->
 	a playlist with some 3-dimensional mandalas and classical music atop.<br />
-&emsp;I'm out of things to put here, because I'm not a cool person <em>en general</em>. <small>(What else did you expect, I do maths for fun)</small><br />
+&emsp;I'm out of things to put here, because I'm not a cool person <em>en general</em>.
+	<small>(What else did you expect, I do maths for fun)</small><br />
 </p>
 
 <p>
@@ -79,16 +86,41 @@ A different question to be asked is
 	then reach me over at anything listed <a href="/" target="_blank">here</a>. I don't have a life so I'll probably respond uncannily quickly.
 </p>
 
-<div>
-<p>
-<span id="show-scans">Show algorithm scans (warning: bloody humongous).</span>
-</p>
+<div class="indented">
+<p></p>
+<strong>"But where's the <em>algorithm</em>?"</strong> – Here's the raw version:<br /> <!--'-->
+<br />
+<br />
+&emsp;MATHJAX(sf "Mandala Generation - Ton" hat(sf "c") sf "i Juri" hat(sf "c") sf "ev-Grgin")<br />
+<br />
+MATHJAX(alpha in NN ∖ {0} - sf "colour space dimensionality (e.g. 3 for RGB, 4 for CMYK, 1 for LUMA)")<br />
+MATHJAX(c nn (: 3, +oo ) - sf "amount of corners in c-gonal mandala base")<br />
+MATHJAX(C - sf "set of point" - sf "corner points of c-gon mandala base")<br />
+MATHJAX(bar C = c)<br />
+<br />
+MATHJAX(n in NN nn (: alpha + 2; +oo ) - sf "current point number")<br />
+MATHJAX(d = ra\nd((:0, c) nn NN) - sf "current chosen corner")<br />
+<br />
+MATHJAX(P_n = (C_d + P_(n-1)) / 2 => a = |C_d P_(n-1)| and b = a/2 = |C_d P_n| = |P_(n-1) P_n|)<br />
+<br />
+MATHJAX(e_m = |P_n P_(n-m)|)<br />
+MATHJAX(E = so\rt(map(f(x) = e_x, NN nn (: 1; alpha + 1 :))))<br />
+<br />
+MATHJAX(colour P_n = map(f(x) = E_x / E_alpha; E ∖ {E_alpha}))<br />
+MATHJAX(or)<br />
+MATHJAX(f = max E)<br />
+MATHJAX(colour P_n = map(f(x) = x / f; E ∖ {f}) => bar(E ∖ {f}) = alpha)<br />
+<br />
+<br />
+(I'm not a real mathematician, so it's most likely not done properly.)<br />
+<br />
+And <span id="show-scans">here</span>'s a scanned handdrawn version with examples (warning, <em>very</em> big, expand at own risk): <!--'-->
+<p></p>
 <div id="scans" class="hidden">
 &emsp;<a href="/capitalism/assets/mandalas/scan-page1.png" target="_blank"><img src="/capitalism/assets/mandalas/scan-page1.png" class="scan" /></a><br />
 &emsp;<a href="/capitalism/assets/mandalas/scan-page2.png" target="_blank"><img src="/capitalism/assets/mandalas/scan-page2.png" class="scan" /></a><br />
 &emsp;<small>Click for big version.</small>
 </div>
-<p></p>
 </div>
 
 
