07dd491779f1e603d5958c35a6d715084e60a5d7
nabijaczleweli <nabijaczleweli@gmail.com>
2017-08-20 16:27:39 +0200

    Make /capitalism/ just redirects

--- capitalism/assets/book_setter.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function() {
-	const EXTENSIONS = ["epub", "mobi", "azw3", "pdf"];
-	const URL_FN = window.custom_book_url || function() {
-		return document.URL.substr(0, document.URL.lastIndexOf('/'));
-	};
-
-	for(let i = 0; i < EXTENSIONS.length; ++i)
-		document.getElementById(EXTENSIONS[i] + "_book_link").href = URL_FN() + "." + EXTENSIONS[i];
-});
--- capitalism/assets/scans_show.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function() {
-	let scans_button = document.getElementById("show-scans");
-	let scans = document.getElementById("scans");
-
-	scans_button.addEventListener("click", function() {
-		scans.classList.toggle("hidden");
-	});
-});
