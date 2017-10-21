4765118fa7b83cb162240c72c2596cfd86a7a72b
nabijaczleweli <nabijaczleweli@gmail.com>
2017-10-21 12:16:08 +0200

    Refont list of rust puns

--- src/extensive_list_of_rust_puns/index.html.pp
+++ src/extensive_list_of_rust_puns/index.html.pp
@@ -9,28 +9,41 @@ work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
 -->
 
 
+#include "../util.h"
 #include "../common.h"
 #include "../writing/writing.h"
 
 
-BOILERPLATE(Extensive List of Rust puns, Puns about the Rust programming language, en-US, <link href="/kaschism/assets/column.css" rel="stylesheet" />)
+#define MATH(...) <span class="math-font-normal roboto-unscale">__VA_ARGS__</span>
+
+#define PUN(pun_id, user_id, user_name, msg_id, msg_time, ...)                                                                                          \
+	<li id=STR(pun_id)>__VA_ARGS__                                                                                                         FORCED_NEWLINE \
+	               — <a href=STR(httpsCOLON_SLASH_SLASHchat.stackoverflow.com/users/user_id)>user_name</a>                                FORCED_NEWLINE \
+	               - <a href=STR(httpsCOLON_SLASH_SLASHchat.stackoverflow.com/transcript/message/msg_id##HASH##msg_id)>MATH(msg_time)</a></li>
+
+
+#define STYLESHEETS <link href="/kaschism/assets/column.css" rel="stylesheet" />                       FORCED_NEWLINE
+                    <link href="../writing/the_taste_of_mi/MathJax-font.css" rel="stylesheet" />       FORCED_NEWLINE \
+                    <link href="../writing/Roboto-font.css" rel="stylesheet" />                        FORCED_NEWLINE \
+                    <link href="../writing/the_taste_of_mi/Merriweather-font.css" rel="stylesheet" />  FORCED_NEWLINE \
+                    <link href="//fonts.googleapis.com/css?family=Droid+Sans+Mono" rel="stylesheet">   FORCED_NEWLINE \
+                    <style>samp { font-family: "Droid Sans Mono", monospace; }</style>
+BOILERPLATE(Extensive List of Rust puns, Puns about the Rust programming language, en-US, STYLESHEETS)
+<span class="roboto">
+
 
 <p></p>
-Extensive list of <a href="//www.rust-lang.org">Rust</a> puns,
+Extensive list of <a href="//rust-lang.org">Rust</a> puns,
 as <a href="//chat.stackoverflow.com/transcript/62927?m=35242527#35242527">requested</a>
 by <a href="//chat.stackoverflow.com/users/2408867">Lukas Kalbertodt</a>:
 <ul>
-  <li id="sharing-Cell-with-threads-but-let-idea-Sync">
-     "Kept wondering what was preventing me from sharing a <samp>Cell</samp> with multiple threads, but then I let the idea <samp>Sync</samp> in."
-     -- <a href="//chat.stackoverflow.com/users/1233251">E_net4</a>
-      - <a href="//chat.stackoverflow.com/transcript/message/35242477#35242477">22.01.17 11:34 PM</a></li>
-  <li id="Indiana-Jones-and-the-Raiders-of-the-Lost-ArcT">"Indiana Jones and the Raiders of the Lost <samp>Arc&lt;T&gt;</samp>"
-                                                          -- <a href="//chat.stackoverflow.com/users/2408867">Lukas Kalbertodt</a>
-                                                           - <a href="//chat.stackoverflow.com/transcript/message/35262129#35262129">23.01.17 11:30 PM</a></li>
-  <li id="one-does-not-simply-implement-Sync">"One does not simply implement <samp>Sync</samp>", on top of a man from Lord Of the Rings
-                                               -- <a href="//chat.stackoverflow.com/users/1233251">E_net4</a>
-                                                - <a href="//chat.stackoverflow.com/transcript/message/39687034#39687034">20.10.17  2:34 PM</a></li>
+	PUN(sharing-Cell-with-threads-but-let-idea-Sync, 1233251, E_net4, 35262129, 23.01.17 11:30 PM,
+	    "Kept wondering what was preventing me from sharing a <samp>Cell</samp> with multiple threads, but then I let the idea <samp>Sync</samp> in.")
+	PUN(Indiana-Jones-and-the-Raiders-of-the-Lost-ArcT, 2408867, Lukas Kalbertodt, 35262129, 20.10.17  11:30 PM,
+	    "Indiana Jones and the Raiders of the Lost <samp>Arc&lt;T&gt;</samp>")
+	PUN(one-does-not-simply-implement-Sync, 1233251, E_net4, 39687034, 20.10.17  2:34 PM,
+	    "One does not simply implement <samp>Sync</samp>", on top of a man from Lord of the Rings)
 </ul>
 
-
+</span>
 BOILERPLATE_END()
