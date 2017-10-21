4c72d00d9a7b6eef46bf7062904cfd4d7aff7068
nabijaczleweli <nabijaczleweli@gmail.com>
2017-10-21 11:51:14 +0200

    One doesn not simply implement Sync, indeed

--- src/extensive_list_of_rust_puns/index.html.pp
+++ src/extensive_list_of_rust_puns/index.html.pp
@@ -20,12 +20,16 @@ Extensive list of <a href="//www.rust-lang.org">Rust</a> puns,
 as <a href="//chat.stackoverflow.com/transcript/62927?m=35242527#35242527">requested</a>
 by <a href="//chat.stackoverflow.com/users/2408867">Lukas Kalbertodt</a>:
 <ul>
-  <li>"Kept wondering what was preventing me from sharing a <samp>Cell</samp> with multiple threads, but then I let the idea <samp>Sync</samp> in."
-       -- <a href="//chat.stackoverflow.com/users/1233251">E_net4</a>
-        - <a href="//chat.stackoverflow.com/transcript/message/35242477#35242477">22.01.17 11:34 PM</a></li>
-  <li>"Indiana Jones and the Raiders of the Lost <samp>Arc&lt;T&gt;</samp>"
-       -- <a href="//chat.stackoverflow.com/users/2408867">Lukas Kalbertodt</a>
-        - <a href="//chat.stackoverflow.com/transcript/message/35262129#35262129">23.01.17 11:30 PM</a></li>
+  <li id="sharing-Cell-with-threads-but-let-idea-Sync">
+     "Kept wondering what was preventing me from sharing a <samp>Cell</samp> with multiple threads, but then I let the idea <samp>Sync</samp> in."
+     -- <a href="//chat.stackoverflow.com/users/1233251">E_net4</a>
+      - <a href="//chat.stackoverflow.com/transcript/message/35242477#35242477">22.01.17 11:34 PM</a></li>
+  <li id="Indiana-Jones-and-the-Raiders-of-the-Lost-ArcT">"Indiana Jones and the Raiders of the Lost <samp>Arc&lt;T&gt;</samp>"
+                                                          -- <a href="//chat.stackoverflow.com/users/2408867">Lukas Kalbertodt</a>
+                                                           - <a href="//chat.stackoverflow.com/transcript/message/35262129#35262129">23.01.17 11:30 PM</a></li>
+  <li id="one-does-not-simply-implement-Sync">"One does not simply implement <samp>Sync</samp>", on top of a man from Lord Of the Rings
+                                               -- <a href="//chat.stackoverflow.com/users/1233251">E_net4</a>
+                                                - <a href="//chat.stackoverflow.com/transcript/message/39687034#39687034">20.10.17  2:34 PM</a></li>
 </ul>
 
 
