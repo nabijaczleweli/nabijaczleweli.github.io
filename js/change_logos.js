9244c2882d5d0c6b99dd845b211c58f493423b07
Karol Świerczek <karol.sw3@gmail.com>
2018-02-22 22:10:26 +0100

    Reduce the code
    This pull request reduces the code and changes syntax to ES6.
    Works only if all <script> tags are put before </body> closing tag.

--- js/change_logos.js
+++ js/change_logos.js
@@ -1,22 +1,7 @@
-window.addEventListener("load", function() {
-	const EXTRA_IMAGES = document.getElementsByClassName("alt-logo-img");
-	EXTRA_IMAGES[0].classList.toggle("active");
-	EXTRA_IMAGES[EXTRA_IMAGES.length - 1].classList.toggle("active");
+const images = document.querySelectorAll(`.alt-logo-img`)
+let activeIndex = 0
 
-	var just_switched = false;
-	Array.from(EXTRA_IMAGES).forEach(function(img, idx) {
-		var next_idx = idx + 1;
-		if(next_idx == EXTRA_IMAGES.length)
-			next_idx = 0;
-
-		// Only triggered on active alt images
-		img.addEventListener("mouseover", function() {
-			if(!just_switched) {
-				img.classList.toggle("active");
-				EXTRA_IMAGES[next_idx].classList.toggle("active");
-				just_switched = true;
-			} else
-				just_switched = false;
-		});
-	});
-});
+document.getElementById(`logo-img-container`).addEventListener(`mouseleave`, ()=>{
+	[...images].map(e => e.classList.remove(`active`))
+	images[++activeIndex % images.length].classList.add(`active`)
+})
