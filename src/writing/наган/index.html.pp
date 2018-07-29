0d21d5f4643ab70f74eedb49991e6a7e7b14f35d
nabijaczleweli <nabijaczleweli@gmail.com>
2018-07-29 07:55:23 +0200

    "Transcribe наган"

---
+++ src/writing/наган/index.html.pp
@@ -0,0 +1,43 @@
+<!--
+nabijaczleweli.xyz (c) by nabijaczleweli@gmail.com (nabijaczleweli)
+​
+nabijaczleweli.xyz is licensed under a
+Creative Commons Attribution 4.0 International License.
+​
+You should have received a copy of the license along with this
+work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
+-->
+
+
+#include "../variable-selectors.h"
+#include "../../common.h"
+#include "../writing.h"
+
+
+<!-- RSS_PUB_DATE: "Wed, 24 Oct 2018 20:12:57 +0200" -->
+
+
+#define STYLESHEETS <link href="/kaschism/assets/column.css" rel="stylesheet" />               FORCED_NEWLINE \
+                    <link href="../Roboto-font.css" rel="stylesheet" />                        FORCED_NEWLINE \
+                    <link href="../the_taste_of_mi/Merriweather-font.css" rel="stylesheet" />
+BOILERPLATE(Наган, Наган, pl, WORD_COUNT_SCRIPT STYLESHEETS)
+
+
+<style>
+#include "../../indent.css"
+#include "../the_taste_of_mi/spacing.css"
+</style>
+
+
+<!-- xx.5.16
+     or xx.4.16 -->
+
+#include "../preface.html"
+<br />
+#include "intro.html"
+#include "non-possumus.html"
+
+
+WORD_COUNTER_END_NON_ENGLISH()
+WRITING_CUSTOM_NAME_STUB_END(this, writing/наган)  <!-- Can't use the auto one because the define degenerates to question marks '-->
+BOILERPLATE_END()
