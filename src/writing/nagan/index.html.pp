ad7306907442e9d8575bda910e9ecf3bf7420ccd
nabijaczleweli <nabijaczleweli@gmail.com>
2018-07-30 07:24:12 +0200

    Add redirexion from nagan to наган

---
+++ src/writing/nagan/index.html.pp
@@ -0,0 +1,22 @@
+<!--
+nabijaczleweli.xyz (c) by nabijaczleweli@gmail.com (nabijaczleweli)
+​
+nabijaczleweli.xyz is licensed under a
+Creative Commons Attribution 4.0 International License.
+​
+You should have received a copy of the license along with this
+work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
+-->
+
+
+#include "../../common.h"
+
+
+BOILERPLATE(Наган, Наган, pl, <meta http-equiv="refresh" content="0;url=/content/writing/наган" />)
+
+
+This content resides at <a href="/content/writing/наган">/content/writing/наган</a>.
+
+
+  </body>
+</html>
