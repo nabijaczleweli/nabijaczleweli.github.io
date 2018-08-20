a472f715cde59ec53568a007fcd496518e0f12f6
nabijaczleweli <nabijaczleweli@gmail.com>
2018-08-20 07:12:23 +0200

    Add poetry root under writing

---
+++ src/writing/poetry/index.html.pp
@@ -0,0 +1,53 @@
+<!--
+nabijaczleweli.xyz (c) by nabijaczleweli@gmail.com (nabijaczleweli)
+​
+nabijaczleweli.xyz is licensed under a
+Creative Commons Attribution 4.0 International License.
+​
+You should have received a copy of the license along with this
+work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
+-->
+
+
+#include "../../common.h"
+
+
+#define MATH(...) <span class="math-font-normal roboto-unscale">__VA_ARGS__</span>
+
+
+#define STYLESHEETS <link href="/kaschism/assets/column.css" rel="stylesheet" />               FORCED_NEWLINE \
+                    <link href="../the_taste_of_mi/MathJax-font.css" rel="stylesheet" />       FORCED_NEWLINE \
+                    <link href="../Roboto-font.css" rel="stylesheet" />                        FORCED_NEWLINE \
+                    <link href="../the_taste_of_mi/Merriweather-font.css" rel="stylesheet" />
+BOILERPLATE((Technically) poetry, Things that make you think‚ from наб and friends, en-GB, STYLESHEETS)
+
+
+<style>
+#include "../the_taste_of_mi/spacing.css"
+#include "../../indent.css"
+</style>
+<span class="roboto">
+
+
+<h2 class="merriweather">(Technically) poetry</h2>
+
+
+<p class="indented continued">
+Sometimes an urge strikes one to create <span class="roboto-em">some</span>thing, others – a topic is suggested and time pressure applied.
+In any case, these things go here, without any warranty.
+</p>
+
+<div>
+	<p></p>
+	One-off:
+	<ul>
+		<li><a href="/content/poetry/">The East Wind</a> —
+			a front-line letter;
+			MATH(19.05.2017).</li>
+	</ul>
+	<p></p>
+</div>
+
+
+</span>
+BOILERPLATE_END()
