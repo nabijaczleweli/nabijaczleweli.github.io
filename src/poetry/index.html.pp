7a9c6c6143403884562da5ed06b7862469937564
nabijaczleweli <nabijaczleweli@gmail.com>
2018-08-21 07:12:22 +0200

    Replace poetry/ with redirect to writing/poetry/the_east_wind/

--- src/poetry/index.html.pp
+++ src/poetry/index.html.pp
@@ -1,31 +1,4 @@
-<!--
-nabijaczleweli.xyz (c) by nabijaczleweli@gmail.com (nabijaczleweli)
-​
-nabijaczleweli.xyz is licensed under a
-Creative Commons Attribution 4.0 International License.
-​
-You should have received a copy of the license along with this
-work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
--->
+#include "../redirect.h"
 
 
-#include "../common.h"
-#include "../fontawesome.h"
-#include "../writing/writing.h"
-
-
-#define HTML_ONLY(...) -->__VA_ARGS__<!--
-
-<!-- RSS_PUB_DATE: "Fri, 19 May 2017 12:25:37 +0200" -->
-
-
-BOILERPLATE(Poetry, Things technically poetic‚ depending whom you ask, en-GB, FONTAWESOME_LINK COLUMN_CSS_LINK WORD_COUNT_SCRIPT)
-
-
-#include "intro.html"
-#include "east_wind.html"
-
-
-WORD_COUNTER_END()
-WRITING_END()
-BOILERPLATE_END()
+REDIRECT(Poetry, Things technically poetic‚ depending whom you ask, en-GB, /content/writing/poetry/the_east_wind/)
