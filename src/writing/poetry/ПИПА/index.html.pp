fdc58c747dff6c6ed305a49dc5f1c315f1fb337a
nabijaczleweli <nabijaczleweli@gmail.com>
2018-08-22 07:21:42 +0200

    "Add ПИПА and first installment thereof"

---
+++ src/writing/poetry/ПИПА/index.html.pp
@@ -0,0 +1,55 @@
+<!--
+nabijaczleweli.xyz (c) by nabijaczleweli@gmail.com (nabijaczleweli)
+​
+nabijaczleweli.xyz is licensed under a
+Creative Commons Attribution 4.0 International License.
+​
+You should have received a copy of the license along with this
+work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
+-->
+
+
+#include "../../variable-selectors.h"
+#include "../../../common.h"
+#include "../../writing.h"
+
+
+<!-- RSS_PUB_DATE: "Sat,  3 Nov 2018 21:03:25 +0100" -->
+
+
+#define STYLESHEETS <link href="../../Roboto-font.css" rel="stylesheet" />                        FORCED_NEWLINE \
+                    <link href="/kaschism/assets/column.css" rel="stylesheet" />                  FORCED_NEWLINE \
+                    <link href="../../the_taste_of_mi/MathJax-font.css" rel="stylesheet" />       FORCED_NEWLINE \
+                    <link href="../../the_taste_of_mi/Merriweather-font.css" rel="stylesheet" />  FORCED_NEWLINE \
+                    <style>                                                                       FORCED_NEWLINE \
+                    .smallcaps {                                                                  FORCED_NEWLINE \
+                    	font-variant: small-caps;                                                   FORCED_NEWLINE \
+                    }                                                                             FORCED_NEWLINE \
+                    </style>
+BOILERPLATE(ПИПА – Dzieła zebrane, Zbiór dzieł Felicji, pl, WORD_COUNT_SCRIPT STYLESHEETS)
+
+
+<style>
+#include "../../../indent.css"
+#include "../../the_taste_of_mi/spacing.css"
+#include "../../translations/sherlock_holmes_the_published_apocrypha/headings.css"
+#include "../../translations/sherlock_holmes_the_published_apocrypha/Roboto-additions.css"
+</style>
+
+
+#include "title.html"
+<h1>&nbsp;</h1>
+#include "contents.html.eppe"
+<h1>&nbsp;</h1>
+#include "introduction.html.eppe"
+<h1>&nbsp;</h1>
+#include "2018.11.02-Homoseksualizm/index.html.eppe"
+#include "2018.11.02-Homoseksualizm/Zawadzka.html.eppe"
+#include "2018.11.02-Homoseksualizm/Micherda.html.eppe"
+#include "2018.11.02-Homoseksualizm/Bujak.html.eppe"
+#include "2018.11.02-Homoseksualizm/nabijaczleweli.html.eppe"
+
+
+WORD_COUNTER_END_NON_ENGLISH()
+WRITING_END()
+BOILERPLATE_END()
