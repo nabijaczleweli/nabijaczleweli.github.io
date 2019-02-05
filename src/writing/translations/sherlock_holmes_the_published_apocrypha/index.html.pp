b3663dce592f176c8e79ce761be9013fe51e8f3f
nabijaczleweli <nabijaczleweli@gmail.com>
2019-02-05 15:35:17 +0100

    Add correct pagebreaks for books

--- src/writing/translations/sherlock_holmes_the_published_apocrypha/index.html.pp
+++ src/writing/translations/sherlock_holmes_the_published_apocrypha/index.html.pp
@@ -19,6 +19,7 @@ work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
 
 #define STYLESHEETS <link href="../../Roboto-font.css" rel="stylesheet" />                        FORCED_NEWLINE \
                     <link href="/kaschism/assets/column.css" rel="stylesheet" />                  FORCED_NEWLINE \
+                    <link href="/kaschism/assets/pagebreak.css" rel="stylesheet" />               FORCED_NEWLINE \
                     <link href="../../the_taste_of_mi/MathJax-font.css" rel="stylesheet" />       FORCED_NEWLINE \
                     <link href="../../the_taste_of_mi/Merriweather-font.css" rel="stylesheet" />  FORCED_NEWLINE \
                     <style>                                                                       FORCED_NEWLINE \
