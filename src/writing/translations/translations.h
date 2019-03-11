e33460369f298f0e8574ef3d12d94d0461d0a1d5
nabijaczleweli <nabijaczleweli@gmail.com>
2019-03-11 07:25:25 +0100

    Add Erotica and SHTPA to translations list

--- src/writing/translations/translations.h
+++ src/writing/translations/translations.h
@@ -15,11 +15,16 @@
 #include "../../util.h"
 
 
-#define TRANSLATION_RAW(semiurl, title, lang, date, ...)                                         \
-	<li><a href=STR(/content/writing/translations/semiurl.html)>title</a> [lang] —  FORCED_NEWLINE \
-				__VA_ARGS__;                                                              FORCED_NEWLINE \
-				<span class="math-font-normal roboto-unscale">date</span>.</li>           FORCED_NEWLINE
+#define TRANSLATION_RAW(semiurl, title, lang, date, ...)                                    \
+	<li><a href=STR(/content/writing/translations/semiurl)>title</a> [lang] —  FORCED_NEWLINE \
+				__VA_ARGS__;                                                         FORCED_NEWLINE \
+				<span class="math-font-normal roboto-unscale">date</span>.</li>      FORCED_NEWLINE
 
 
-#define TRANSLATIONS_LIST() \
-	TRANSLATION_RAW(funeral_blues, Funeral Blues (ew. Pogrzebowy Blues), PL, 19.04.2018, Translation of W. H. Auden's<!--'--> "Funeral Blues" for a contest)
+#define TRANSLATIONS_LIST()                                                                                                                                          \
+	TRANSLATION_RAW(funeral_blues.html, Funeral Blues (ew. Pogrzebowy Blues),                                                                                          \
+		              PL, 19.04.2018, Translation of W. H. Auden's<!--'--> "Funeral Blues" for a contest)                                                                \
+	TRANSLATION_RAW(sherlock_holmes_the_published_apocrypha/, Sherlock Holmes，the Published Apocrypha (ew. Sherlock Holmes，Opublikowane Apokryfy),                    \
+	                PL, 07.12.2017–WIP, Ongoing effort to translate the entirety of "Sherlock Holmes, the Published Apocrypha"，currently roughly on page 22 from 330)  \
+	TRANSLATION_RAW(erotica, Erotica,                                                                                                                                  \
+		              PL, 18.11.2018–WIP, Ongoing translation of some of my faves from Reynolds'<!--'--> "Erotica")
