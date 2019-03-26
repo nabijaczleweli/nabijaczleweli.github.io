e18004c33bb9281d985baf73b77dd5fb61e58ec0
nabijaczleweli <nabijaczleweli@gmail.com>
2019-03-26 07:25:21 +0100

    Add basic wiggly-circle
    Still needs the code, probably doing something with the footer, and a
    mention in parent index

---
+++ src/maths/wiggly-circle.html.pp
@@ -0,0 +1,46 @@
+<!--
+nabijaczleweli.xyz (c) by nabijaczleweli@gmail.com (nabijaczleweli)
+​
+nabijaczleweli.xyz is licensed under a
+Creative Commons Attribution 4.0 International License.
+​
+You should have received a copy of the license along with this
+work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
+-->
+
+
+#include "../common.h"
+
+
+<!-- RSS_PUB_DATE: "Tue, 13 Jun 2017 22:42:51 +0200" -->
+
+#define PIECE(pid, fname, ...)                                                   \
+	<div class="art-box">                                                          \
+		<h4 class="merriweather label">__VA_ARGS__</h4>                              \
+		<a href=STR(HASH##pid)>                                                      \
+			<img id=#pid src=STR(/content/assets/wiggly-circle/output_##fname.png) />  \
+		</a>                                                                         \
+	</div>
+
+#define LINKS <link href="/content/assets/unobtrusive-art.css" rel="stylesheet" />               \
+              <link href="../writing/the_taste_of_mi/Merriweather-font.css" rel="stylesheet" />
+BOILERPLATE(wiggly-circle, A selexion of circles‚ most of them wiggly‚ some of them colourful, en-GB, LINKS)
+
+
+<div class="art-box"><h2 class="merriweather">wiggly-circle</h2></div>
+
+
+PIECE(lines                                      , lines_2x_w1.5,                                       the return to the fight)
+PIECE(r0_pts                                     , r0_pts,                                              centrism)
+PIECE(r0_lines                                   , r0_lines_2x_w1.5,                                    pointless breadth)
+PIECE(r0_lines_40__2                             , r0_lines_2x_w1.5_40__2,                              volumous)
+PIECE(r0_lines_1600x900_40                       , r0_lines_2x_w1.5_1600x900_40,                        breathe)
+PIECE(r0_pts_o.8_1600x900_60                     , r0_pts_2x_w1.5_o.8_1600x900_60,                      pointedly degenerative)
+PIECE(r0_lines_rgba_1600x900_60                  , r0_lines_2x_w1.5_rgba_1600x900_60,                   trans-gressive)
+PIECE(r0_sngllines_rgba_1600x900_60              , r0_sngllines_2x_w1.5_rgba_1600x900_60,               pointedly home-of-sexual)
+PIECE(r0_skiplines_rgba_1600x900_60              , r0_skiplines_2x_w1.5_rgba_1600x900_60,               drop-ped)
+PIECE(r0_skiplines_comp-step_rgba_1600x900_60__2 , r0_skiplines_2x_w1.5_comp-step_rgba_1600x900_60__2,  c o m p e n s a t e)
+PIECE(r0_skiplines2..4_comp-step_rgba_1600x900_60, r0_skiplines2..4_2x_w1.5_comp-step_rgba_1600x900_60, {2,3,4}-drop)
+
+
+BOILERPLATE_END()
