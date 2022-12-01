ef6e530dba3cc60214328629d3f4c3a44a41af2a
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-12-01 17:18:43 +0100

    Add 009-XEROX-SIGMA-9-balls. Update mail for x32 comments

--- src/blogn_t/index.html.pp
+++ src/blogn_t/index.html.pp
@@ -60,6 +60,7 @@ Not banged out in an hour.
 	POST(006-UNIX-r-ATT,                         Thu, 08 Jul 2021 20:26:38 +0200, 006. ¹UNIX® is a registered trademark of AT&T.)
 	POST(007-groff-Tps-cyrillic-et-al,           Sat, 28 Aug 2021 18:36:44 +0200, 007. Cleanly enabling Cyrillic and broad Unicode output in groff -Tps)
 	POST(008-amix-fonts,                         Thu, 17 Nov 2022 22:36:33 +0100, 008. <span class="topaz">AMIX</span> <span class="sunfont">fonts</span>)
+	POST(009-XEROX-SIGMA-9-balls,                Thu, 01 Dec 2022 17:16:50 +0100, 009. XEROX SIGMA 9 balls)
 	<!-- Remember to update latest post in toplevel index -->
 </span>
 
--- src/index.html.pp
+++ src/index.html.pp
@@ -14,21 +14,8 @@ work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
 
 #define STYLESHEETS <link href="/kaschism/assets/column.css" rel="stylesheet" />                    FORCED_NEWLINE \
                     <link href="writing/Roboto-font.css" rel="stylesheet" />                        FORCED_NEWLINE \
-                    <link href="writing/the_taste_of_mi/Merriweather-font.css" rel="stylesheet" />  FORCED_NEWLINE \
-                    <link href="/content/assets/blogn_t/008.01-amix-fonts.css" rel="stylesheet" />
+                    <link href="writing/the_taste_of_mi/Merriweather-font.css" rel="stylesheet" />
 BOILERPLATE(main(), набꞌs web-site, en-GB, STYLESHEETS)
-<style>
-/* TODO: remove when we remove 008! */
-.sunfont {
-	font-family: sunfont;
-	font-size: 1.25em; /* 60/48 */
-}
-
-.topaz {
-	font-family: topaz;
-	font-size: 0.80em; /* 55/69 */
-}
-</style>
 
 
 <h2 class="merriweather">наб'<!--'-->s web-site, hi!</h2>
@@ -43,8 +30,8 @@ BOILERPLATE(main(), набꞌs web-site, en-GB, STYLESHEETS)
 	Become cursed by my <a href="blogn_t/">blog</a>…<br />
 	Latest post:<br />
 	<samp style="display: inline-block; margin-left: 1em;" class="roboto-unscale">
-		<a href="blogn_t/008-amix-fonts.html">008. <span class="topaz">AMIX</span> <span class="sunfont">fonts</span></a>;
-		<span style="white-space: nowrap;">Thu, 17 Nov 2022 22:36:33 +0100</span>
+		<a href="blogn_t/009-XEROX-SIGMA-9-balls.html">009. XEROX SIGMA 9 balls</a>;
+		<span style="white-space: nowrap;">Thu, 01 Dec 2022 17:16:50 +0100</span>
 	</samp>
 </p>
 
