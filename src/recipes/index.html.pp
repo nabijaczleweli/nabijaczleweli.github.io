919e5d4a6888a73f70159a9759df32174f1e3f19
наб <nabijaczleweli@nabijaczleweli.xyz>
2023-05-04 00:04:57 +0200

    Add Corn freamed (fried steamed) rolls (buns), rose-shaped

--- src/recipes/index.html.pp
+++ src/recipes/index.html.pp
@@ -25,11 +25,15 @@ work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
 BOILERPLATE(Recipes, My answer to the replicaiton crisis, en-GB, STYLESHEETS)
 
 #define NUM(...) <span class="roboto-unscale math-font-normal">__VA_ARGS__</span>
+#define REPLICATION(date, url) <a href=STR(httpsCOLON_SLASH_SLASHurl)>NUM(date)</a>
 
 
 <style>
 #include "../writing/the_taste_of_mi/spacing.css"
-#include "../indent.css"
+
+details {
+	margin-top: 1em;
+}
 </style>
 
 <script>
@@ -50,17 +54,17 @@ MAIN_HEADING(My answer to the replication crisis)
 <span class="roboto">
 
 <p>
-	Latest replication: <a href="https://cohost.org/nabijaczleweli/post/1440206-leek-multi-laye">NUM(2023-05-03)</a>.
+	Latest replication: REPLICATION(2023-05-03, cohost.org/nabijaczleweli/post/1440206-leek-multi-laye)</a>.
 	<button id="togglupa">Toggle all</button>
 </p>
 
 <details open>
 	<summary class="merriweather roboto-unscale">Leek multi-layer cake</summary>
 
-	<p class="continued">
+	<p class="continuing">
 		<a href="//youtu.be/2rNbXgnnyQg">Leek multi-layer cake is delicious and no skill</a>,
 		by Haijuan Food,
-		replicated <a href="https://cohost.org/nabijaczleweli/post/1440206-leek-multi-laye">NUM(2023-05-03)</a>:</p>
+		replicated REPLICATION(2023-05-03, cohost.org/nabijaczleweli/post/1440206-leek-multi-laye):</p>
 	<ol class="continuing">
 		<li>NUM(500g) of flour, NUM(~2g) of salt, flock with NUM(150g) of boiling water then NUM(130g) of normal</li>
 		<li>NUM(60g) egg – I had good results with an uncompensated NUM(70g) one</li>
@@ -68,7 +72,7 @@ MAIN_HEADING(My answer to the replication crisis)
 		<li>roll into cylinder, cut into NUM(8) guys, spin each guy flat then ball him; they all sleep on a tray for NUM(1h), oiled and plasticked</li>
 		<li>with oiled counter, stick, and hands, roll each guy into like a NUM(2:5) rectangle</li>
 		<li>"not too thin", but you do want it quite thin, presumably the difference here is "not paper-thin"</li>
-		<li>dispense gloop, omit NUM(~1-1.5cm) from the edge, fold thrice, roll and press the loose ends toward the face with the last fold</li>
+		<li>dispense gloop, omit NUM(~1-1.5cm) from the edge, fold thrice (NUM(4) sections), roll and press the loose ends toward the face with the last fold</li>
 		<li>these go on a hot oiled leccy pan, weld side down, NUM(~1min) on the first side until they set, then flip 1/min for NUM(~6min) total –
 		    filling ought to be visible from the edges here but may not be if the dough's thick and you don't have much contrast in the gloop</li>
 	</ol>
@@ -87,6 +91,62 @@ MAIN_HEADING(My answer to the replication crisis)
 		<li>a NUM(15g) miso packet</li>
 		<li>soy sus, fish sus, salt</li>
 	</ol>
+</details>
+
+<details open>
+	<summary class="merriweather roboto-unscale">Corn freamed (fried steamed) rolls (buns), rose-shaped</summary>
+
+	<p class="continuing">
+		<a href="//youtu.be/7tZEh6uzcOo">Cornmeal for rolls? Easy to make fluffy and soft</a>,
+		by Haijuan Food,
+		replicated REPLICATION(2023-04-29, cohost.org/nabijaczleweli/post/1413956-okay-these-are-pog-c),
+		           REPLICATION(2023-05-02, cohost.org/nabijaczleweli/post/1433335-little-secondary-ris),
+		           REPLICATION(2023-05-02, cohost.org/nabijaczleweli/post/1434488-okay-these-ones-rose):</p>
+	<ol class="continuation">
+		<li>NUM(200g) of <a href="//cohost.org/nabijaczleweli/post/1404618-this-user-just-got">corn</a>,
+		    flock with NUM(200g) of boiling water,
+		    let cool until not hot to second knuckle – with the eggs you want to get a good yeast temp</li>
+		<li>NUM(3g) of yeast, spoon of sugar, two eggs – mine are NUM(~70g)</li>
+		<li>NUM(200g) of flour – maybe my eggs are huge, maybe idk, but starting with NUM(250g) and likely adding slightly more to get to a reasonable "dough" consistency
+		    — instead of a paste, which doesn'<!--'-->t rise; the spec is to <em>maybe add water</em> which is insane to me —
+		    worked for me to avoid the REPLICATION(2023-05-02, cohost.org/nabijaczleweli/post/1433335-little-secondary-ris) incident again</li>
+		<li>flock, accrue, knead in bowl for NUM(5min), he sleeps covered for NUM(40min)</li>
+		<li>knead a bit on floured counter to de-gas, roll into cylinder, cut into NUM(~12) guys –
+		    there's a critical mass above which they won't re-rise(?), and NUM(9) left them too big
+		    (again, REPLICATION(2023-05-02, cohost.org/nabijaczleweli/post/1433335-little-secondary-ris)),
+		    NUM(13) and NUM(12) worked very well, and she does NUM(12)</li>
+		<li>for each guy:
+			<ol>
+				<li>spin spin him flat – thus averaging the crust that'<!--'-->s inevitably developed</li>
+				<li>cylindrise, flatten, roll out long –
+				    you want a high aspect ratio and relatively thin;
+				    if he sticks to the counter then unstick him, flour the counter, and zoom him around so he'<!--'-->s free for the next step</li>
+				<li>start a cut through the middle NUM(~.5-1cm) from one edge, and through the other</li>
+				<li>"braid him like a braid", i.e. keep passing one over the other,
+				                                   keep the top-facing faces facing the top and the counter-facing faces facing the counter</li>
+				<li>at the end, weld the halves together again – the best-kept secret here is that no matter how badly you do any of this it'<!--'-->ll turn out great</li>
+				<li>flip (dry side up wet side down now),
+				    roll from the welded bit to the uncut bit,
+				    stick the tongue that'<!--'-->s left to the side, may need water assist</li>
+				<li>ass-down spin him around a bit to ensure flat perpendicular ass</li>
+				<li>hide under a cover</li>
+			</ol>
+			  – this took me like NUM(50) minutes the first time, and like NUM(40) the third
+			</li>
+		<li>oil a pan, lay them there, they sleep covered for NUM(15min)</li>
+		<li>crank to medium for three minutes –
+		    maybe her leccy pan is more powerful than my stove, I need like NUM(1min) on 9 then NUM(4min) on 6,
+		    you'<!--'-->re shooting for a well-developed brown crust on the ass</li>
+		<li>pour water NUM(⅓)<sup>rd</sup> way up – the guys? the pan? idk. – kill the heat, they steam until the water gets sucked off (NUM(~10min))</li>
+	</ol>
+	<p>
+		These came out just about perfect REPLICATION(2023-04-29, cohost.org/nabijaczleweli/post/1413956-okay-these-are-pog-c),
+		but the water level is finnicky, and pouring in more and turning the heat back on turns out badly for me
+		(stuck to bottom; REPLICATION(2023-05-02, cohost.org/nabijaczleweli/post/1433335-little-secondary-ris),
+		                  REPLICATION(2023-05-02, cohost.org/nabijaczleweli/post/1434488-okay-these-ones-rose)).
+		Maybe this is easier if you have a plastic pan the right size, but my biggest (NUM(Φ245)) is stainless (but the right size).
+		Operator error.
+	</p>
 </details>
 
 
