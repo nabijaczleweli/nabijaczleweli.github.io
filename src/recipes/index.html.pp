79a5a6217f025371ac4769806dc1bd3484a20a43
наб <nabijaczleweli@nabijaczleweli.xyz>
2023-06-18 00:20:23 +0200

    maanto

--- src/recipes/index.html.pp
+++ src/recipes/index.html.pp
@@ -14,7 +14,7 @@ work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
 #include "../common.h"
 
 
-<!-- RSS_PUB_DATE: "Thu, 15 Jun 2023 18:59:26 +0200" -->
+<!-- RSS_PUB_DATE: "Sun, 18 Jun 2023 00:19:44 +0200" -->
 
 
 #define STYLESHEETS MAIN_HEADING_STYLE FONTAWESOME_LINK                                               FORCED_NEWLINE \
@@ -95,7 +95,7 @@ MAIN_HEADING(My answer to the replication crisis)
 <span class="roboto">
 
 <p>
-	Latest replication: REPLICATION(2023-06-15, cohost.org/nabijaczleweli/post/1688384-scale-comparison)</a>.
+	Latest replication: REPLICATION(2023-06-17, cohost.org/nabijaczleweli/post/1705325-i-don-t-know-what-i)</a>.
 	<button toggle-tag="details">Toggle all</button>
 	<button toggle-tag="center" >Toggle images</button>
 </p>
@@ -111,7 +111,8 @@ IMAGUS(staging.cohostcdn.org/attachment/7e10cb46-e68f-454c-8a1f-920b6f7ef7a3/DSC
 		                                 (<span lang="zh-CN"><!--"-->开花馒头好吃方法很简单，学会这个做法，蓬松柔软个个开花【海娟美食】<!--"--></span>)</a>,
 		by Haijuan Food,
 		replicated REPLICATION(2023-06-13, cohost.org/nabijaczleweli/post/1676107-didn-t-explode-along),
-	             REPLICATION(2023-06-15, cohost.org/nabijaczleweli/post/1688384-scale-comparison):</p>
+	             REPLICATION(2023-06-15, cohost.org/nabijaczleweli/post/1688384-scale-comparison),
+	             REPLICATION(2023-06-17, cohost.org/nabijaczleweli/post/1705325-i-don-t-know-what-i):</p>
 	<ol class="continuation">
 		<li>NUM(500g) of flour, NUM(4-5g) of yeast, NUM(35g) of sugar, NUM(50-60g) egg, mix</li>
 		<li>flock in NUM(80g) of yogurt – I'<!--'-->ve been informed chinese yogurt is closer to zsiadłe mleko (or, indeed, lait ribot). that worked fine</li>
