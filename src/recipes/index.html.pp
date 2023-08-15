54445019651c9951a15a8ef8bf051aad7832f2a7
наб <nabijaczleweli@nabijaczleweli.xyz>
2023-08-15 03:53:14 +0200

    Half-rye inflating bread

--- src/recipes/index.html.pp
+++ src/recipes/index.html.pp
@@ -15,7 +15,7 @@ work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
 
 
    <!-- RSS_PUB_DATE: "Wed, 3 May 2023 23:16:48 +0200" -->
-<!-- RSS_UPDATE_DATE: "Wed, 02 Aug 2023 17:26:21 +0200" -->
+<!-- RSS_UPDATE_DATE: "Tue, 15 Aug 2023 03:50:50 +0200" -->
 
 
 #define STYLESHEETS MAIN_HEADING_STYLE FONTAWESOME_LINK                                               FORCED_NEWLINE \
@@ -96,10 +96,34 @@ MAIN_HEADING(My answer to the replication crisis)
 <span class="roboto">
 
 <p>
-	Latest replication: REPLICATION(2023-08-02, $101010.pl/@nabijaczleweli/110820714260955866)</a>.
+	Latest replication: PREPLICATION(2023-08-14)</a>.
 	<button toggle-tag="details">Toggle all</button>
 	<button toggle-tag="center" >Toggle images</button>
 </p>
+
+
+IMAGUS(storage.waw.cloud.ovh.net/v1/AUTH_74714a37e6e24c7fb695d79be309da62/101010public/media_attachments/files/110/885/534/443/986/044/original/3a0c63fc3d47d289.jpg, $101010.pl/@nabijaczleweli/110885535194714458,
+       storage.waw.cloud.ovh.net/v1/AUTH_74714a37e6e24c7fb695d79be309da62/101010public/media_attachments/files/110/885/534/928/580/898/original/1711d463c0ae032f.jpg, $101010.pl/@nabijaczleweli/110885535194714458)
+
+<details open>
+	SUMMARY(supposed-laobing, Half-rye inflating bread)
+	<p class="continuing">
+		<a href="//youtu.be/y_hT7nzXzGE" lang="zh-CN"><!--"-->这样烙饼好神奇！不抹油不包油酥，个个空心像气球，简单好吃<!--"--></a>,
+		by Yi Ning'<!--'-->s Mother Loves Food,
+		replicated REPLICATION(2023-08-10, $101010.pl/@nabijaczleweli/110867561340637439),
+		           REPLICATION(2023-08-11, $101010.pl/@nabijaczleweli/110874141044133521),
+		           REPLICATION(2023-08-12, $101010.pl/@nabijaczleweli/110877700083516193),
+		           REPLICATION(2023-08-13, $101010.pl/@nabijaczleweli/110885535194714458):</p>
+	<ol class="continuation">
+		<li>NUM(100g) of rye flour, NUM(190g) of boiling water, pastify, let cool to yeast temp</li>
+		<li>mix in NUM(3g) of yeast</li>
+		<li>NUM(200g) of flour, knead</li>
+		<li>he sleeps</li>
+		<li>cylindrise, divide NUM(6)-wise</li>
+		<li>for each guy: knead radially, ballify</li>
+		<li>for each guy: flatten directly, flour bilaterally, roll out to NUM(2-3mm), bake on pan</li>
+	</ol>
+</details>
 
 
 ONE_IMAGUS(storage.waw.cloud.ovh.net/v1/AUTH_74714a37e6e24c7fb695d79be309da62/101010public/media_attachments/files/110/809/495/805/550/457/original/20bef8e45b221fe0.jpg, $101010.pl/@nabijaczleweli/110809682480516115)
@@ -277,7 +301,8 @@ IMAGUS(lfs.nabijaczleweli.xyz/0016-cohost-export/httpsCOLON_SLASH_SLASHstaging.c
 		           REPLICATION(2023-06-17, lfs.nabijaczleweli.xyz/0016-cohost-export/httpsCOLON_SLASH_SLASHcohost.org/nabijaczleweli/post/1705325-i-don-t-know-what-i),
 		           REPLICATION(2023-06-28, lfs.nabijaczleweli.xyz/0016-cohost-export/httpsCOLON_SLASH_SLASHcohost.org/nabijaczleweli/post/1776589-they-made),
 		           REPLICATION(2023-06-30, lfs.nabijaczleweli.xyz/0016-cohost-export/httpsCOLON_SLASH_SLASHcohost.org/nabijaczleweli/post/1791054-empty),
-		           REPLICATION(2023-07-18, $101010.pl/@nabijaczleweli/110736025902684639):</p>
+		           REPLICATION(2023-07-18, $101010.pl/@nabijaczleweli/110736025902684639),
+		           PREPLICATION(2023-08-14):</p>
 	<ol class="continuation">
 		<li>NUM(500g) of flour, NUM(4-5g) of yeast, NUM(35g) of sugar, NUM(50-60g) egg, mix</li>
 		<li>flock in NUM(80g) of yogurt – I'<!--'-->ve been informed chinese yogurt is closer to zsiadłe mleko (or, indeed, lait ribot). that worked fine</li>
