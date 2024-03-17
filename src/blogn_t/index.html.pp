d9b9c10db677251da68c1a4108e56c926de8919c
наб <nabijaczleweli@nabijaczleweli.xyz>
2024-03-17 04:21:23 +0100

    Align titles for a/b/c groups

--- src/blogn_t/index.html.pp
+++ src/blogn_t/index.html.pp
@@ -20,6 +20,7 @@ work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
 #define POST(post_id, post_time, post_time_2, ...)  \
 	<p></p>PPOST(post_id, post_time, post_time_2, __VA_ARGS__)
 
+#define B(n) n##.<span style="white-space: pre-wrap;"><!--"-->  <!--"--></span>
 
 #define STYLESHEETS MAIN_HEADING_STYLE                                                                 FORCED_NEWLINE \
                     <link href="/kaschism/assets/column.css" rel="stylesheet" />                       FORCED_NEWLINE \
@@ -64,7 +65,7 @@ Not banged out in an hour. <button style="float: right;" onclick="reverse();">Re
 	 POST(003-maircl-poc,                          Sat, 11 Jul 2020 00:34:50 +0200, 003. A proof-of-concept IRC/mail bridge)
 	 POST(004-cinque,                              Sat, 18 Jul 2020 22:14:33 +0200, 004. New album: cinque)
 <p></p>
-	PPOST(005-low-curse-zfs-on-root,               Mon, 14 Sep 2020 00:20:20 +0200, 005. Low-curse ZFS-on-root for new Debian installations on multi-disk systems)
+	PPOST(005-low-curse-zfs-on-root,               Mon, 14 Sep 2020 00:20:20 +0200, B(005)Low-curse ZFS-on-root for new Debian installations on multi-disk systems)
 	PPOST(005a-zfs-on-root-single-disk-corollary,  Thu, 05 Nov 2020 22:06:30 +0100, 005a. A single-disk but slightly more cursed ZFS-on-root corollary)
 	 POST(006-UNIX-r-ATT,                          Thu, 08 Jul 2021 20:26:38 +0200, 006. ¹UNIX® is a registered trademark of AT&T.)
 	 POST(007-groff-Tps-cyrillic-et-al,            Sat, 28 Aug 2021 18:36:44 +0200, 007. Cleanly enabling Cyrillic and broad Unicode output in groff -Tps)
@@ -73,7 +74,7 @@ Not banged out in an hour. <button style="float: right;" onclick="reverse();">Re
 	 POST(010-tme-Sun-2-SunOS3-2023,               Mon, 19 Jun 2023 23:53:58 +0200, 010. tme booting SunOS 3 on Sun-2 in 2023)
 	 POST(011-linux-splice-exclusion,              Fri, 07 Jul 2023 01:42:34 +0200, 011. pipe exclusion with splice() under Linux)
 <p></p>
-	PPOST(012-debian-chromebook-lenovo-300e-gen2,  Mon, 08 Jan 2024 16:06:34 +0100, 012. Debian on the Lenovo 300e 2<sup>nd</sup>-gen arm64 Chromebook)
+	PPOST(012-debian-chromebook-lenovo-300e-gen2,  Mon, 08 Jan 2024 16:06:34 +0100, B(012)Debian on the Lenovo 300e 2<sup>nd</sup>-gen arm64 Chromebook)
 	PPOST(012a-installing-debian-chromebook-lenovo-300e-gen2-intel, Thu, 11 Jan 2024 04:04:28 +0100, 012a. Installing Debian on the Lenovo 300e 2<sup>nd</sup>-gen Chromebook (Intel))
 	PPOST(012b-debian-chromebook-lenovo-300e-gen2-mtk-real, Wed, 07 Feb 2024 01:09:33 +0100, 012b. Debian on the Lenovo 300e 2<sup>nd</sup>-gen MTK arm64 Chromebook (hana) (real))
 	<!-- Remember to update latest post in toplevel index -->
