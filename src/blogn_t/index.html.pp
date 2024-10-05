b89ecf59488d5c51ab0457f9289af671071a3140
наб <nabijaczleweli@nabijaczleweli.xyz>
2024-10-05 22:07:19 +0200

    installed locally so masked :/

--- src/blogn_t/index.html.pp
+++ src/blogn_t/index.html.pp
@@ -28,6 +28,7 @@ work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
                     <link href="../writing/the_taste_of_mi/Merriweather-font.css" rel="stylesheet" />  FORCED_NEWLINE \
                     <link href="//fonts.googleapis.com/css?family=Droid+Sans+Mono" rel="stylesheet">   FORCED_NEWLINE \
                     <link href="/content/assets/blogn_t/008.01-amix-fonts.css" rel="stylesheet" />     FORCED_NEWLINE \
+                    <link href="/content/assets/blogn_t/016.01-BSDi-fonts.css" rel="stylesheet" />     FORCED_NEWLINE \
                     <style>samp { font-family: "Droid Sans Mono", monospace; }</style>
 
 #undef linux
